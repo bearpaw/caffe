9b6174aac729baac6863a7c49a1a92e257c2babd
Wei Yang <platero.yang@gmail.com>
2015-05-15 09:52:01 +0800

    demo matlab window data

--- matlab/demo_window_data.m
+++ matlab/demo_window_data.m
@@ -1,5 +1,5 @@
 % demo usage of window data layer
 quietmode = 1; % whether to print information while load window data file
-parser = window_data.DB('../../chen-nips14-pose-dev/cache/lsp/window_data/lsp_val.txt', quietmode);
+parser = window_data.DB('../../chen-nips14-pose-dev/cache/lsp/window_data/lsp_train.txt', quietmode);
 db = parser.get_db();
 parser.show_window(100); % visualize 10 images
